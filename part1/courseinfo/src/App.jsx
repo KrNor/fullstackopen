10f323eefe9394c18b2d1daae1579be2e726a844
Kristijonas <krnorkus@gmail.com>
2025-01-20 14:15:43 +0200

    fixed 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part} exercise={props.exercises} />
-      <Part part={props.part} exercise={props.exercises} />
-      <Part part={props.part} exercise={props.exercises} />
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
     </>
   );
 };
@@ -41,9 +41,14 @@ const App = () => {
   return (
     <div>
       <Header name={course}></Header>
-      <Content part={part1} exercise={exercises1}></Content>
-      <Content part={part2} exercise={exercises2}></Content>
-      <Content part={part3} exercise={exercises3}></Content>
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      ></Content>
       <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}></Total>
     </div>
   );
