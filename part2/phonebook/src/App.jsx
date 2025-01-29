7aad53fb24629256b379bcd740d59d0b60fc513c
Kristijonas <krnorkus@gmail.com>
2025-01-29 15:59:35 +0200

    2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const PhoneBook = (props) => {
   return (
@@ -47,16 +48,20 @@ const Number = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setnewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
-  const [currId, setCurrId] = useState(persons[persons.length - 1].id);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
+  const [currId, setCurrId] = useState(4); //persons[persons.length - 1].id;
   const changeName = (event) => {
     event.preventDefault();
     setNewName(event.target.value);
