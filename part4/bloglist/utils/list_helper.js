203e8eb124dd41f71a836c59b1f9270e13201b8e
Kristijonas <krnorkus@gmail.com>
2025-02-11 11:29:19 +0200

    4.4 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  //   console.log(blogs);
+
+  var asd = blogs
+    .map((element) => {
+      //   console.log(element.likes);
+      return element.likes;
+    })
+    .reduce((a, b) => a + b, 0);
+  //   console.log(asd);
+  return asd;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
