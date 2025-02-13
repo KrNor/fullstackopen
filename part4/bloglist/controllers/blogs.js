ebb9f2f83bc43fa80f0268c083a9db2e3b42491a
Kristijonas <krnorkus@gmail.com>
2025-02-13 12:25:45 +0200

    4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,31 @@
 const BlogRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 var ObjectId = require("mongoose").Types.ObjectId;
 
 BlogRouter.get("/", async (request, response, next) => {
-  const blog = await Blog.find({});
+  const blog = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
   response.json(blog);
 });
 
 BlogRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const tempCreator = await User.findById("67ada2b3518a033051b0ec31");
+  console.log(tempCreator.id);
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: tempCreator.id,
   });
   const ress = await blog.save();
+  tempCreator.blogs.push(ress.id);
+  await tempCreator.save();
+  console.log(tempCreator.notes);
   // console.log(ress);
   if (ress) {
     response.status(201).json(ress);
