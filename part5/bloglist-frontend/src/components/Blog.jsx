893f70727a55cea75d589ca55a974da4e0d1e620
Kristijonas <krnorkus@gmail.com>
2025-02-17 17:03:07 +0200

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [blogShown, setBlogShown] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  if (!blogShown) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}{" "}
+        <button
+          onClick={() => {
+            setBlogShown(true);
+          }}
+        >
+          show
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div style={blogStyle}>
+        <p>{blog.title}</p> <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}
+          <button>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+        <button
+          onClick={() => {
+            setBlogShown(false);
+          }}
+        >
+          hide
+        </button>
+      </div>
+    );
+  }
+};
+
+export default Blog;
