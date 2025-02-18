f13e5f0143f05a9d119cc99f5a75159abb71db3e
Kristijonas <krnorkus@gmail.com>
2025-02-18 11:09:54 +0200

    5.10 done, 5.9 was done last commit

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -120,9 +120,11 @@ const App = () => {
         />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} errorHandler={handleError} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} errorHandler={handleError} />
+        ))}
     </div>
   );
 };
