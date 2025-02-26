f2417664bf0aa317e5291c79343c528e0ef812c2
Kristijonas <krnorkus@gmail.com>
2025-02-26 11:11:27 +0200

    6.1 and 6.2 done

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,23 +1,41 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  // console.log(action);
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+    case "GOOD":
+      const newGoodState = {
+        ...state,
+        good: state.good + 1,
+      };
+      // console.log(newGoodState);
+      return newGoodState;
+    case "OK":
+      const newOkState = {
+        ...state,
+        ok: state.ok + 1,
+      };
+      return newOkState;
+    case "BAD":
+      const newBadState = {
+        ...state,
+        bad: state.bad + 1,
+      };
+      return newBadState;
+    case "ZERO":
+      const newNewState = {
+        good: 0,
+        ok: 0,
+        bad: 0,
+      };
+      return newNewState;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
