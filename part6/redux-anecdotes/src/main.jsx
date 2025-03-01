ac027c8fa3a36c38a056b60a62b2e4a493cd8138
Kristijonas <krnorkus@gmail.com>
2025-03-01 10:31:03 +0200

    6.16 done

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,12 +2,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
-import anecdoteService from "./services/anecdotes";
-import { setAnecdotes } from "./reducers/anecdoteReducer";
+// import anecdoteService from "./services/anecdotes";
+// import { setAnecdotes } from "./reducers/anecdoteReducer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
-anecdoteService.getAll().then((notes) => store.dispatch(setAnecdotes(notes)));
+// anecdoteService.getAll().then((notes) => store.dispatch(setAnecdotes(notes)));
