dc316a515cf5bb76d43837eeddb04ec494324765
Kristijonas <krnorkus@gmail.com>
2025-03-03 10:09:32 +0200

    6.20 done and start of query-anecdotes

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,53 @@
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  // const anecdotes = [
+  //   {
+  //     content: "If it hurts, do it more often",
+  //     id: "47145",
+  //     votes: 0,
+  //   },
+  // ];
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+    retry: 3,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
+  if (result.isLoading || (!result.isLoading && result.isError)) {
+    return (
+      <div>anecdote service is unavaliable due to problems in the sever</div>
+    );
+  }
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
