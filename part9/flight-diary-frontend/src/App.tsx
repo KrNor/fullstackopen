6858a0fcd718f45aa366821f554e00af3d730432
Kristijonas <krnorkus@gmail.com>
2025-04-14 13:32:55 +0300

    9.18 done, works but not proud if this

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,12 +1,102 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const baseUrl = "http://localhost:3001/api/diaries";
 interface Diary {
   id: number;
   date: string;
   weather: string;
   visibility: string;
   comment?: string;
+}
+
+interface NewDiary {
+  date: string;
+  weather: string;
+  visibility: string;
+  comment?: string;
+}
+
+const createDiary = async (object: NewDiary) => {
+  return axios.post<NewDiary>(baseUrl, object).then((response) => {
+    // console.log("a diary was added!");
+    console.log(response.data);
+  });
+};
+
+function AddDiary() {
+  const [dateValue, setDateValue] = useState<string>("");
+  const [weatherValue, setWeatherValue] = useState<string>("");
+  const [visibilityValue, setVisibilityValue] = useState<string>("");
+  const [commentValue, setCommentValue] = useState<string>("");
+
+  const SubmitTheDiary = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    // console.log("button was clicked");
+
+    const diaryObject = {
+      date: dateValue,
+      weather: weatherValue,
+      visibility: visibilityValue,
+      comment: commentValue,
+    };
+
+    try {
+      const returnedDiary = await createDiary(diaryObject);
+      console.log(returnedDiary);
+
+      setDateValue("");
+      setWeatherValue("");
+      setVisibilityValue("");
+      setCommentValue("");
+      window.location.reload();
+    } catch (error: unknown) {
+      // this will be changed later
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={SubmitTheDiary}>
+        <div>
+          <div>
+            <p>the date is:</p>
+            <input
+              type="text"
+              value={dateValue}
+              onChange={({ target }) => setDateValue(target.value)}
+            />
+          </div>
+          <p>the visibility is:</p>
+          <div>
+            <input
+              type="text"
+              value={visibilityValue}
+              onChange={({ target }) => setVisibilityValue(target.value)}
+            />
+          </div>
+          <p>the weather is:</p>
+          <div>
+            <input
+              type="text"
+              value={weatherValue}
+              onChange={({ target }) => setWeatherValue(target.value)}
+            />
+          </div>
+          <p>comment is:</p>
+          <div>
+            <input
+              type="text"
+              value={commentValue}
+              onChange={({ target }) => setCommentValue(target.value)}
+            />
+          </div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 function App() {
@@ -32,6 +122,7 @@ function App() {
 
   return (
     <div>
+      <AddDiary />
       {diaries.map((element: Diary) => {
         if (element.comment) {
           return (
